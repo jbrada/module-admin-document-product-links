8f9fcaeb3f8084c65e2de651e7ce211b0d598575
Jiří Brada <jiri@jbrada.cz>
2022-03-05 15:48:21 +0100

    success test (to squash)

---
+++ Test/Integration/Model/Query/IsProductExistingTest.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace JBrada\AdminOrderProductLinks\Test\Integration\Model\Query;
+
+use JBrada\AdminOrderProductLinks\Model\Query\IsProductExisting;
+use Magento\TestFramework\Helper\Bootstrap;
+use PHPUnit\Framework\TestCase;
+use TddWizard\Fixtures\Catalog\ProductBuilder;
+use TddWizard\Fixtures\Catalog\ProductFixture;
+
+class IsProductExistingTest extends TestCase
+{
+    /**
+     * A random high entity_id that does not exist in Magento
+     */
+    private const NOT_EXISTING_PRODUCT_ID = 9999999999;
+
+    /**
+     * @var IsProductExisting
+     */
+    private $isProductExisting = null;
+
+    protected function setUp(): void
+    {
+        $this->isProductExisting = Bootstrap::getObjectManager()->get(IsProductExisting::class);
+    }
+
+    public function testSuccess()
+    {
+        $this->assertTrue(true);
+    }
+
+}
